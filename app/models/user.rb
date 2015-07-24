2e5e2b4c439368189d0db0c7dbd620a29f1692e4
Jacob Rogers <JacobRogers42@gmail.com>
2015-07-24 11:08:51 -0700

    Basic user structure set up with BCrypt

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  include BCrypt
+
+  validates :username, presence: true, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+end
